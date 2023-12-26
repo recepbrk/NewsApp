17a3ea8358658d2b61fbf68faa6ed13a266de90d
Recep Güzel <recepgzelr@gmail.com>
2023-12-26 16:29:10 +0300

    commit with love <3

--- app/src/main/java/com/example/newsappproject/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/newsappproject/ui/profile/ProfileFragment.kt
@@ -1,22 +1,56 @@
 package com.example.newsappproject.ui.profile
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
-import com.example.newsappproject.R
+import com.example.newsappproject.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        nightMode()
+
+    }
+
+    private fun nightMode() {
+        val sharedPreferences = requireActivity().getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val nightMode = sharedPreferences.getBoolean("night", false)
+
+        if (nightMode) {
+            binding.switchMode.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        }
+
+
+        binding.switchMode.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (!isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("night", false)
+                editor.apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("night", true)
+                editor.apply()
+            }
+        }
     }
 
 }
